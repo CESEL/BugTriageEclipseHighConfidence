d36623633c6423fe9024a6876c6aa9aceaa78f79
aindrila1989 <to.aindrila1989@gmail.com>
2019-12-08 23:58:07 -0500

    Create install-libs.sh

---
+++ install-libs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip install gitpython
+pip install pydriller
+pip install nltk
+pip install numpy
+pip install pandas
+pip install python-bugzilla
+pip install python-dateutil
+pip install scikit-learn
+pip install scipy
